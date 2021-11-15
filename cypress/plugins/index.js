6ef50ceb98bdc9e0d1ec6534fee81ef48d552bf3
Sergio <sergioloporto@gmail.com>
2021-11-15 09:36:51 +0100

    added cpress

---
+++ cypress/plugins/index.js
@@ -0,0 +1,55 @@
+// Need to use require in plugins file
+// https://stackoverflow.com/questions/59743577/is-there-anyway-to-query-mongodb-in-cypress-test
+// const MongoClient = require('mongodb').MongoClient;
+// const url = 'mongodb://localhost:27017/';
+
+// module.exports = (on: any, config: any) => {
+//     on('task', {
+//         clearCasesDB() {
+//             return new Promise((resolve, reject) => {
+//                 MongoClient.connect(
+//                     url,
+//                     { useUnifiedTopology: true },
+//                     async (error, db) => {
+//                         if (error) reject(error);
+//                         const covid19db = db.db('covid19');
+//                         await covid19db.collection('cases').deleteMany({});
+//                         db.close();
+//                         resolve(null);
+//                     },
+//                 );
+//             });
+//         },
+//         clearSourcesDB() {
+//             return new Promise((resolve, reject) => {
+//                 MongoClient.connect(
+//                     url,
+//                     { useUnifiedTopology: true },
+//                     async (error, db) => {
+//                         if (error) reject(error);
+//                         const covid19db = db.db('covid19');
+//                         await covid19db.collection('sources').deleteMany({});
+//                         db.close();
+//                         resolve(null);
+//                     },
+//                 );
+//             });
+//         },
+//         clearUsersDB() {
+//             return new Promise((resolve, reject) => {
+//                 MongoClient.connect(
+//                     url,
+//                     { useUnifiedTopology: true },
+//                     async (error, db) => {
+//                         if (error) reject(error);
+//                         const covid19db = db.db('covid19');
+//                         await covid19db.collection('users').deleteMany({});
+//                         await covid19db.collection('sessions').deleteMany({});
+//                         db.close();
+//                         resolve(null);
+//                     },
+//                 );
+//             });
+//         },
+//     });
+// };
