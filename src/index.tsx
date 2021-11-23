c6ba6c1c894a7f248a0456c97920b7867221dce2
Sergio <sergioloporto@gmail.com>
2021-11-23 10:29:47 +0100

    exported global styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,7 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createGlobalStyle } from 'styled-components';
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-`;
+import {GlobalStyle} from './theme/globalStyles';
 
 ReactDOM.render(
     <StrictMode>
