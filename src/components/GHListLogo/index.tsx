341ec1ff141a02639ff5c135d9649124fb9ad391
Maciej Zarzeczny <maciej.zarzeczny@htdevelopers.com>
2022-03-08 15:45:09 +0100

    Revert "Added Ukraine logo (#38)"
    This reverts commit b9da3d10c6e2e5beb0cf4fd15a53133ad1610d3f.

--- src/components/GHListLogo/index.tsx
+++ src/components/GHListLogo/index.tsx
@@ -1,29 +1,16 @@
-import logo from 'assets/images/ukraine_logo.svg';
-import { LogoStyles, LogoImage, LogoText } from './styled';
-import { Link } from 'react-router-dom';
+import logo from 'assets/images/gh_logo.svg';
+import { LogoStyles, LogoImage } from './styled';
 import MapGuide from 'components/MapGuide';
 
 export default function GHListLogo(): JSX.Element {
-    const env = process.env.NODE_ENV;
-
     return (
         <LogoStyles id="logo">
-            <div id="logo-container">
-                <a
-                    href={
-                        env === 'production'
-                            ? 'https://global.health/'
-                            : 'http://dev-globalhealth.pantheonsite.io/'
-                    }
-                >
+            <a href="https://global.health/">
+                <div id="logo-container">
                     <LogoImage src={logo} />
-                </a>
-
-                <Link to="/country">
-                    <LogoText className="logoText">Map</LogoText>
-                </Link>
-            </div>
-
+                    <span className="logoText">Map</span>
+                </div>
+            </a>
             <MapGuide />
         </LogoStyles>
     );
